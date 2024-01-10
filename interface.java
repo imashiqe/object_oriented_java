1b0c02f1a113ef671b33b45019aafe5b35dd3222
Ashiqur Rahman <imashiqe@gmail.com>
2024-01-10 21:34:24 +0600

    Interface

---
+++ interface.java
@@ -0,0 +1,25 @@
+interface Animal {
+    int eyes =2;
+     void walk();
+
+}
+
+interface Herbivore {
+
+}
+
+class  Horse  implements Animal {
+     public  void  walk(){
+         System.out.println("Walks  on  4 legs");
+     }
+}
+
+
+
+public class {
+     public static void main(String args[])
+     {
+         Horse horse = new Horse();
+         
+     }
+}
