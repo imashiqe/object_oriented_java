8fbdc953d1687fab42725e9a4369825065f46d19
Ashiqur Rahman <imashiqe@gmail.com>
2024-01-19 23:03:15 +0600

    hashcode

---
+++ string/string.java
@@ -0,0 +1,14 @@
+public class string {
+
+
+     // String  str  = "hello";
+     //
+
+     public static void main(String[] args)
+      {
+         char[] strChar = {'h', 'e', 'l', 'l', 'o'};
+           String str = "hello";
+       
+         System.out.println(str.hashCode());
+      }
+}
