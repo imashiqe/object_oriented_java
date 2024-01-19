bb925b3e30cea72076ab9db99b2e90102740d734
Ashiqur Rahman <imashiqe@gmail.com>
2024-01-19 23:28:14 +0600

    string method

--- string/string.java
+++ string/string.java
@@ -11,4 +11,25 @@ public class string {
        
          System.out.println(str.hashCode());
       }
+
+        //Hello World 
+        //lowercase it
+        //uppercase it
+        //reverse it
+        //find the length
+        //find the index of 'e'
+        //find the index of 'l'
+        //find substring from index 1 to 3
+        //replace 'l' with 'm'
+        //replace vs replaceAll
+        //split it by ' '
+        //trim it
+        //concat it with " world" (with method)
+        //contains
+        //equals
+        //equalsIgnoreCase
+        //compareTo
+        // == vs equals
+        //difference between isBlank and isEmpty
+        //starts with, ends with
 }
